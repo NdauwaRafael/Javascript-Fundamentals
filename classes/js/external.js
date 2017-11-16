414193f795ffe6e313245d6a1459ecc32c12faec
NdauwaRafael <raphaelndauwa@gmail.com>
2017-11-16 20:06:20 +0300

    Iteration and promises

---
+++ classes/js/external.js
@@ -0,0 +1,33 @@
+function favourites(){
+  var top_movies = {
+
+          "1": {
+                       "name":        "Thor: Ragnarok ",
+                       "station":     "2017",
+                       "year":        "CBS",
+                       "ratings":     "8.2"
+          },
+            "2": {
+                       "name":        "Murder on the Orient Express",
+                       "station":     "2017",
+                       "year":        "CBS",
+                       "ratings":     "6.8"
+          },
+          "3": {
+                       "name":        "Justice League",
+                       "station":     "2017",
+                       "year":        "USA",
+                       "ratings":     "8.0"
+          },
+          "4": {
+                       "name":        "Spider-Man: Homecoming ",
+                       "station":     "2017",
+                       "year":        "SYFY",
+                       "ratings":     "7.6"
+          }
+        }
+
+        return top_movies;
+}
+
+export {favourites};
